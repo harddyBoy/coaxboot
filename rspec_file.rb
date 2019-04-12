bb20ff244f5be4cdbd6d1c0039560a7b1c46fe84
harddyBoy <bqkapko@gmail.com>
2019-04-12 14:47:47 +0300

    add TDD file

---
+++ rspec_file.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require_relative "../jsonParsh/simple.rb"
+
+describe Parser do
+	let(:parser) { Parser.top_headlines('us') }
+
+=begin
+	shared_examples "exist file"  do 
+		it 'should exist JSON file' do
+			expect(File.exist? 'responseFile.json').to eq(true)
+		end
+	end
+=end
+ # don't work
+
+	it 'should be instance of Parser class' do
+       	expect(parser).to be_a(Parser) 
+	end
+
+	describe '#top_headlines' do
+       it 'should save response to api_response var' do
+       		expect(parser.api_response).not_to be_nil
+       end
+	end
+
+	describe '#write_file' do
+		it 'should write to file' do
+			parser.write_file
+			expect(File.exist? 'responseFile.json').to eq(true)
+			# it_behaves_like "exist File"
+		end
+	end
+
+	describe '#read_file' do
+			it 'should read from file' do
+			parser.read_file
+			expect(File.exist? 'responseFile.json').to eq(true)
+		end
+	end
+end
--- simple (копія).rb
+++ simple (копія).rb
@@ -6,7 +6,7 @@ class Parser
   API_KEY = '882e10dd2b474a23bb7a3efa85e66b61'.freeze
   base_uri 'https://newsapi.org/v2/'
 
-  attr_reader :api_response
+  attr_reader :var
  
 
   def self.top_headlines(country_name) # метод класу
@@ -19,31 +19,31 @@ class Parser
 
   def top_headlines # метод екземпляру
     response = self.class.get("/top-headlines", @options).parsed_response
-    @api_response = response
+    @var = response
    	write_file
     self
   end
 
-  def write_file
-    if File.exist?('responseFileFalse.json')
+    def write_file
+      if File.exist?('responseFileFalse.json')
         puts "Sorry - I would be overwriting a file"
-    else
-       file = File.new('responseFile.json', 'w')
-       file.write(JSON.pretty_generate(api_response))
-       read_file
-       file.close
+      else
+        file = File.new('responseFile.json', 'w')
+        file.write(JSON.pretty_generate.response)
+        read_file
+        file.close
+      end
     end
-  end
 
-  def read_file
-    puts JSON.pretty_generate(api_response)
-  end
-  
+    def read_file
+    	puts JSON.pretty_generate
+    end
 end
 
-#Parser.top_headlines('US')
+Parser.top_headlines('US')
 parser = Parser.top_headlines('us')
-p parser.api_response
+p parser.var
+
 p parser.write_file
 #p parser.read_file
 
